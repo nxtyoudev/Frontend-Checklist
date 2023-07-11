b3a64eee123bedb84655c635bf3208dd0cde4417
topdev86 <topdev86@gmail.com>
2023-07-11 06:00:12 -0700

    change button size on landing page

--- components/trust/index.js
+++ components/trust/index.js
@@ -14,7 +14,7 @@ export default () => (
             bearbeiten können und Dich anschließend kontaktieren können.</p>
           <div className='flex gap-4 md:gap-8 mt-12 flex-col md:flex-row'>
             <a href="https://formular.mg-pools.de/start/" target="_blank">
-              <button className='md:w-auto w-full text-center md:text-left flex items-center md:justify-start justify-center text-white gap-4 bg-[#1E75EE] md:pl-4 md:pr-8 py-5'>
+              <button className='md:w-auto w-full text-center md:text-left flex items-center md:justify-start justify-center text-white gap-4 bg-[#1E75EE] md:pl-4 md:pr-8 py-5 border border-[#1E75EE]'>
                 <span className='text-[14px]'>HIER ENTLANG</span>
                 <svg width="39" height="12" viewBox="0 0 39 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M38.5303 6.53033C38.8232 6.23744 38.8232 5.76256 38.5303 5.46967L33.7574 0.696699C33.4645 0.403806 32.9896 0.403806 32.6967 0.696699C32.4038 0.989593 32.4038 1.46447 32.6967 1.75736L36.9393 6L32.6967 10.2426C32.4038 10.5355 32.4038 11.0104 32.6967 11.3033C32.9896 11.5962 33.4645 11.5962 33.7574 11.3033L38.5303 6.53033ZM0 6.75H38V5.25H0V6.75Z" fill="white"/>
